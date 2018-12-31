7ebb401a866be863a4c5aaf04a5cdae7a1247ae1
Diego <A01227808@gmail.com>
2018-12-31 12:57:53 -0600

    removed personal user and pass

--- conf.js
+++ conf.js
@@ -1,9 +1,9 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-  user: 'postgres', //your user in your postgres db
+  user: '', //your user in your postgres db
   host: 'localhost',
   database: 'store',
-  password: 'holkeano10x10', //the password you have in your postgres db
+  password: '', //the password you have in your postgres db
   port: 5432,
 });
 
